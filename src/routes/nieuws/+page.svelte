dbdbdd4607e6f91c8211e3d2cfdedbdf7dbcf729
Peter Felis <peter@felis.nl>
2022-09-07 22:03:04 +0200

    animatieenzo

---
+++ src/routes/nieuws/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import Bericht from "$lib/Bericht.svelte";
+
+    export let data;
+</script>
+
+<aside id="nieuws" class="pagina nieuwsVoordelen ">
+    <div class="nieuwsveld breed">
+        <h2 class="bold">Het laatste nieuws</h2>
+            <div class='nieuwsoverzicht'>
+            {#await data then data}
+            {#if data.info.status!='error'}              
+                    {#each data.info as item}
+                        <Bericht {item}/>
+                    {/each}
+                
+            {/if}
+            {/await}
+            </div>
+            <a href='/#nieuws'>
+            <h5 class="ctatext">
+            <img class='omdraaien' src="images/Icon awesome-long-arrow-alt-right-1.svg" alt="" />
+            terug
+            </h5>
+            </a>
+    </div>
+</aside>
+
+<style>
+    .breed{
+        min-width: 80vw;      
+    }
+    .nieuwsoverzicht{
+        width:100%;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    
+
+    .omdraaien{
+        transform: scale(-1, 1);
+
+    }
+</style>
