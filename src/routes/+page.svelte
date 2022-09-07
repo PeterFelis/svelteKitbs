792b632e99b76d95b1ee7d532dc8afd1c090e6de
Peter Felis <Peter@felis.nl>
2022-09-07 18:04:32 +0200

    def (hoop ik)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,4 @@
 <script>
-    // outhand 07-09-20220
-    // svelte kit. one page
-    // hosting vercel
-    // scss (geen framework)
-    // responsive: 4 maten
-    // zelfgemaakt JS mobile menu met animerende knoppen
-    // zelfgemaakte js slider
-    // koppeling (server side) met nieuws api
-    // haalt bij ieder bezoek de laatse 4 nieuws items op en toont deze
-    //
-    //
-    // peter felis
-    //
-    //
-
     import { onMount } from "svelte/internal";
     import scriptJS from "$lib/script";
 
